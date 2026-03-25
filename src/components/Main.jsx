4f0f617c5c04f63325d5a536250440985c97b8be
Henrique <henriquelucio95@hotmail.com>
2026-03-25 19:37:56 -0300

    Section to show ingredients and recipe

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,22 +2,20 @@ import React from "react"
 
 export default function Main(){
 
-
     const [ingredients, setIngredients] = React.useState([])
+
     let ingredientsListItems = ingredients.map(ingredient => (
         <li key={ingredient}>{ingredient}</li>
     ))
 
-    function addIngredients(event){
-        event.preventDefault()
-        const formData = new FormData(event.currentTarget)
+    function addIngredients(formData){
         const newIngredient = formData.get("ingredient")
         setIngredients(prevIngredients => [...prevIngredients, newIngredient])
     }
 
     return (
         <main>
-            <form onSubmit={addIngredients} className="add-ingredient-form">
+            <form action={addIngredients} className="add-ingredient-form">
                 <input 
                     aria-label="Add ingredient" 
                     type="text" 
@@ -25,9 +23,16 @@ export default function Main(){
                     name="ingredient"  />
                 <button>+ Add Ingredient</button>
             </form>
-            <ul>
-                {ingredientsListItems}
-            </ul>
+            <section>
+                <h2>Ingredients on hand:</h2>
+                <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
+                <div className="get-recipe-container">
+                    <h3>Ready for a recipe?</h3>
+                    <p>Generate a recipe from your list of ingredients.</p>
+                </div>
+                <button>Get a recipe</button>
+            </section>
+
         </main>
     )
 }
